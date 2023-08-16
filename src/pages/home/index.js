4c60919640dd93f05c6a4ce0360a4328d6eb4bcf
davidgamaserrate1 <david.gama@ufms.br>
2023-08-16 09:03:32 -0300

    feat(Home) tela inicial

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import BodyComponent from "../../components/bodyComponent";
 import PictureModule from "../../components/pictureModule";
 import Information from "../../components/information";
+import './home-styles.css'
 
 const Home = ()=>{
     return(
     <BodyComponent>
         <PictureModule/>        
-        <Information information="David Serrate"/>
+        <div className="informations">
+            <Information  information="David Serrate"/>
+            <Information  information="Full stack developer"/>
+            <Information  information="Campo grande, MS"/>
+        </div>
     </BodyComponent>
     )
     
